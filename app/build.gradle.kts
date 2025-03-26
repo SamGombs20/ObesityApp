c663b79b921404305ae0c05428123c9e598aedf3
SamGombs20 <gombajosh18@gmail.com>
2025-03-26 15:00:44 +0300

    Update:Blog item

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose.android)
     implementation (libs.retrofit)
     implementation(libs.converter.gson)
+    implementation(libs.coil.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/josh/obesityapp/presentation/components/BlogComponent.kt
@@ -0,0 +1,62 @@
+package com.josh.obesityapp.presentation.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import com.josh.obesityapp.data.model.BlogType
+import com.josh.obesityapp.ui.theme.customBrown
+import com.josh.obesityapp.ui.theme.customDarkGreen
+
+@Composable
+fun BlogItem(blog: BlogType, onClick: () -> Unit){
+    val previewText = blog.body.firstOrNull()?.children?.firstOrNull()?.text?: ""
+    ElevatedCard(
+        onClick = onClick,
+        modifier = Modifier.fillMaxWidth(),
+        elevation = CardDefaults.elevatedCardElevation(
+            defaultElevation = 4 .dp
+        )
+    ) {
+        Row {
+            blog.mainImage?.asset?.url?.let {
+                AsyncImage(
+                    model = it,
+                    contentDescription = null,
+                    modifier = Modifier.weight(0.3f),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Spacer(Modifier.width(8 .dp))
+            Column {
+                Text(
+                    text = blog.title,
+                    fontWeight = FontWeight(600),
+                    color = customBrown
+                )
+                if (previewText.isNotEmpty()){
+                    Text(
+                        text = previewText,
+                        maxLines = 2,
+                        overflow = TextOverflow.Ellipsis,
+                        color = customDarkGreen,
+                        fontWeight = FontWeight(400)
+                    )
+                }
+            }
+
+        }
+    }
+}
