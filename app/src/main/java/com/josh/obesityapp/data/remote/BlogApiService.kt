a708b7546bd387435ba2b9b9c4e82e9152152b38
SamGombs20 <gombajosh18@gmail.com>
2025-03-26 12:21:36 +0300

    Update:Setting up sanity for blog screen

--- app/src/main/java/com/josh/obesityapp/data/remote/BlogApiService.kt
+++ app/src/main/java/com/josh/obesityapp/data/remote/BlogApiService.kt
@@ -1,6 +1,25 @@
 package com.josh.obesityapp.data.remote
 
+import com.josh.obesityapp.data.model.Blog
+import retrofit2.http.GET
+import retrofit2.http.Headers
+
 interface BlogApiService {
+    @Headers(
+        "Content-Type: application/json",
+        "Authorization: Bearer sktntqHWzf2isaoQDA6QsZhMooJR7lYwyH6JPEEsQFHmhmBbK1JjyBXnU5tbPJ5OcFcR8t14rHaHhChTqAcRSagEQajUPNKRVlEluJ9Zsw94qcZPP0kx48BcwNif82TBpt2vd8oWH89iCRO0hL4bozQsFonaZpCPpqYodOumFLHOS3l66Qxo"
+    )
+    @GET("v2025-03-26/data/query/production?query=" +
+            "*[_type == \"blogPost\"]{\n" +
+            "                _id,\n" +
+            "                title,\n" +
+            "                slug,\n" +
+            "                body,\n" +
+            "                mainImage,\n" +
+            "                publishedAt,\n" +
+            "                author->{_id, name, bio, image}\n" +
+            "            }")
+        suspend fun getBlogs(): List<Blog>
+
 
 }
-//sktntqHWzf2isaoQDA6QsZhMooJR7lYwyH6JPEEsQFHmhmBbK1JjyBXnU5tbPJ5OcFcR8t14rHaHhChTqAcRSagEQajUPNKRVlEluJ9Zsw94qcZPP0kx48BcwNif82TBpt2vd8oWH89iCRO0hL4bozQsFonaZpCPpqYodOumFLHOS3l66Qxo
---
+++ app/src/main/java/com/josh/obesityapp/data/repository/BlogRepository.kt
@@ -0,0 +1,4 @@
+package com.josh.obesityapp.data.repository
+
+class BlogRepository {
+}
