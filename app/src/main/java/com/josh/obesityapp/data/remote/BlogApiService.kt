9d8665d551c1eaeb764095e5dcc949f9b83638f4
SamGombs20 <gombajosh18@gmail.com>
2025-03-26 13:04:44 +0300

    Update:Setting up sanity for blog screen

--- app/src/main/java/com/josh/obesityapp/data/remote/BlogApiService.kt
+++ app/src/main/java/com/josh/obesityapp/data/remote/BlogApiService.kt
@@ -10,16 +10,7 @@ interface BlogApiService {
         "Content-Type: application/json",
         "Authorization: Bearer sktntqHWzf2isaoQDA6QsZhMooJR7lYwyH6JPEEsQFHmhmBbK1JjyBXnU5tbPJ5OcFcR8t14rHaHhChTqAcRSagEQajUPNKRVlEluJ9Zsw94qcZPP0kx48BcwNif82TBpt2vd8oWH89iCRO0hL4bozQsFonaZpCPpqYodOumFLHOS3l66Qxo"
     )
-    @GET("v2025-03-26/data/query/production?query=" +
-            "*[_type == \"blogPost\"]{\n" +
-            "                _id,\n" +
-            "                title,\n" +
-            "                slug,\n" +
-            "                body,\n" +
-            "                mainImage,\n" +
-            "                publishedAt,\n" +
-            "                author->{_id, name, bio}\n" +
-            "            }")
+    @GET("v2025-03-26/data/query/production?query=*[_type == 'blogPost']")
         suspend fun getBlogs(): Response<List<Blog>>
 
 
