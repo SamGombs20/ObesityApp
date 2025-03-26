96367977c68623ab5538c658b7d96690acc13865
SamGombs20 <gombajosh18@gmail.com>
2025-03-26 13:55:53 +0300

    Update:Setting up sanity for blog screen

--- app/src/main/java/com/josh/obesityapp/data/remote/BlogApiService.kt
+++ app/src/main/java/com/josh/obesityapp/data/remote/BlogApiService.kt
@@ -5,13 +5,16 @@ import com.josh.obesityapp.data.model.SanityResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Query
 
 interface BlogApiService {
     @Headers(
         "Content-Type: application/json",
         "Authorization: Bearer sktntqHWzf2isaoQDA6QsZhMooJR7lYwyH6JPEEsQFHmhmBbK1JjyBXnU5tbPJ5OcFcR8t14rHaHhChTqAcRSagEQajUPNKRVlEluJ9Zsw94qcZPP0kx48BcwNif82TBpt2vd8oWH89iCRO0hL4bozQsFonaZpCPpqYodOumFLHOS3l66Qxo"
     )
-    @GET("v2025-03-26/data/query/production?query=*[_type == 'blogPost']{_id, title, slug, body, mainImage{ asset->{url} }, author->{_id, name, bio}, publishedAt}")
-    suspend fun getBlogs(): Response<SanityResponse>
+    @GET("v2025-03-26/data/query/production")
+    suspend fun getBlogs(
+        @Query("query") query: String = "*[_type == 'blogPost']{_id, title, slug, body, mainImage{ asset->{url} }, author->{_id, name, bio}, publishedAt}"
+    ): Response<SanityResponse>
 
 }
