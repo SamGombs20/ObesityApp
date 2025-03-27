69f03ef9209e53cfef1de6c695e31b05b52c0f9b
SamGombs20 <gombajosh18@gmail.com>
2025-03-27 15:19:23 +0300

    Update:UI modifications

--- app/src/main/java/com/josh/obesityapp/presentation/view/HomeScreen.kt
+++ app/src/main/java/com/josh/obesityapp/presentation/view/HomeScreen.kt
@@ -24,9 +24,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.josh.obesityapp.R
 import com.josh.obesityapp.navigation.NestedNavigationItem
