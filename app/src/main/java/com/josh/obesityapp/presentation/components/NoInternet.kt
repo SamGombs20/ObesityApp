631ce5382b536f704cbaa7fd09e8e9e945962be0
SamGombs20 <gombajosh18@gmail.com>
2025-03-28 13:35:47 +0300

    Update:UI modification!

--- app/src/main/java/com/josh/obesityapp/presentation/components/NoInternet.kt
+++ app/src/main/java/com/josh/obesityapp/presentation/components/NoInternet.kt
@@ -1,7 +1,10 @@
 package com.josh.obesityapp.presentation.components
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -10,26 +13,47 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.josh.obesityapp.R
+import com.josh.obesityapp.ui.theme.customBrown
+import com.josh.obesityapp.ui.theme.customDarkGreen
 
 @Composable
 fun NoInternet(){
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
-        Image(
-            painter = painterResource(R.drawable.no_wifi),
-            contentDescription = stringResource(R.string.no_internet),
-            modifier = Modifier.size(
-                width = 200.dp,
-                height = 200.dp
+    Column (modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally){
+        Box(
+            modifier = Modifier.size(200 .dp)
+        ){
+            Image(
+                painter = painterResource(R.drawable.no_wifi),
+                contentDescription = stringResource(R.string.no_internet),
+                modifier = Modifier.matchParentSize()
             )
-        )
+            Canvas(
+                modifier = Modifier.matchParentSize()
+            ) {
+                val gradient = Brush.linearGradient(
+                    colors = listOf(
+                        customBrown, customDarkGreen
+                    )
+                )
+                drawRect(
+                    brush = gradient
+                )
+            }
+        }
         Spacer(Modifier.height(16 .dp))
         Text(
-            text = stringResource(R.string.no_internet),
-            modifier = Modifier.align(Alignment.BottomCenter)
+            text = stringResource(R.string.no_internet)
         )
     }
+}
+@Preview(showBackground = true)
+@Composable
+fun NoInternetPreview(){
+    NoInternet()
 }
