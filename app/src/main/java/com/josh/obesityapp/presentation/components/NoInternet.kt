0781f48a1d4456a07546becc170d626d452a5bb8
SamGombs20 <gombajosh18@gmail.com>
2025-03-28 14:23:19 +0300

    Update:Implementing lazy loading for images!

--- app/src/main/java/com/josh/obesityapp/presentation/components/NoInternet.kt
+++ app/src/main/java/com/josh/obesityapp/presentation/components/NoInternet.kt
@@ -1,6 +1,5 @@
 package com.josh.obesityapp.presentation.components
 
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -9,11 +8,15 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,32 +26,33 @@ import com.josh.obesityapp.ui.theme.customBrown
 import com.josh.obesityapp.ui.theme.customDarkGreen
 
 @Composable
-fun NoInternet(){
-    Column (modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally){
-        Box(
-            modifier = Modifier.size(200 .dp)
-        ){
-            Image(
-                painter = painterResource(R.drawable.no_wifi),
-                contentDescription = stringResource(R.string.no_internet),
-                modifier = Modifier.matchParentSize()
+fun NoInternet() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        val gradientBrush = Brush.linearGradient(
+            colors = listOf(customDarkGreen, customBrown)
+        )
+        Image(
+            painter = painterResource(R.drawable.no_wifi),
+            contentDescription = stringResource(R.string.no_internet),
+            modifier = Modifier
+                .size(200.dp)
+                .graphicsLayer(alpha = 0.99f)
+                .drawWithCache {
+                    onDrawWithContent {
+                        drawContent()
+                        drawRect(gradientBrush, blendMode = BlendMode.SrcAtop)
+                    }
+                },
+
             )
-            Canvas(
-                modifier = Modifier.matchParentSize()
-            ) {
-                val gradient = Brush.linearGradient(
-                    colors = listOf(
-                        customBrown, customDarkGreen
-                    )
-                )
-                drawRect(
-                    brush = gradient
-                )
-            }
-        }
-        Spacer(Modifier.height(16 .dp))
+        Spacer(Modifier.height(16.dp))
         Text(
-            text = stringResource(R.string.no_internet)
+            text = stringResource(R.string.no_internet),
+            color = MaterialTheme.colorScheme.error
         )
     }
 }
