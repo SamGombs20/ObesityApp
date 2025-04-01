4149ff9af774180250799f2cf561ace553b0bc61
SamGombs20 <gombajosh18@gmail.com>
2025-04-01 11:53:11 +0300

    Update:UI modifications

--- app/src/main/java/com/josh/obesityapp/presentation/view/BlogDetails.kt
+++ app/src/main/java/com/josh/obesityapp/presentation/view/BlogDetails.kt
@@ -1,17 +1,61 @@
 package com.josh.obesityapp.presentation.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
+import coil3.request.crossfade
 import com.josh.obesityapp.presentation.viewmodel.BlogViewModel
 
 @Composable
 fun BlogDetailsScreen(blogViewModel: BlogViewModel){
     val selectedBlog by blogViewModel.selectedBlog.collectAsState()
+    val imageUrl = selectedBlog?.mainImage?.asset?.url ?: ""
+    var isLoading by remember { mutableStateOf(true) }
     if (selectedBlog != null){
-        Text(
-            text = selectedBlog!!.title
-        )
+        Column(
+            modifier = Modifier.padding(16 .dp)
+        ) {
+            Text(
+                text = selectedBlog!!.title,
+                fontSize = 22 .sp,
+                fontWeight = FontWeight(600)
+            )
+            Spacer(modifier = Modifier.height(16 .dp))
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(imageUrl)
+                    .crossfade(true)
+                    .listener(
+                        onStart = { isLoading = true },
+                        onSuccess = { _, _ -> isLoading = false },
+                        onError = { _, _ -> isLoading = false }
+                    )
+                    .build(),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxWidth().height(200 .dp).clip(
+                    shape = RoundedCornerShape(8 .dp)
+                )
+            )
+        }
     }
 }
